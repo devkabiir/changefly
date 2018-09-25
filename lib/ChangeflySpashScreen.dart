06e2c1b0efbdf2a4cf87a529e6065b2a84cd1993
devkabiir <dev@kabiir.me>
2018-09-25 13:35:35 +0530

    :lipstick: (splash-screen) add changefly-name fade-in animation

--- lib/ChangeflySpashScreen.dart
+++ lib/ChangeflySpashScreen.dart
@@ -1,5 +1,46 @@
 import 'package:flutter/material.dart';
 
+class ChangeflyName extends StatefulWidget {
+  @override
+  _ChangeflyNameState createState() => _ChangeflyNameState();
+}
+
+class _ChangeflyNameState extends State<ChangeflyName> with SingleTickerProviderStateMixin {
+  AnimationController animationController;
+  Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(vsync: this, duration: Duration(seconds: 2));
+    animation = CurvedAnimation(curve: Curves.linear, parent: animationController)
+      ..addListener(() {
+        setState(() {});
+      });
+
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 0.0),
+        child: FadeTransition(
+          opacity: animation,
+          child: Image.asset('assets/changefly-name.png'),
+        ),
+      ),
+    );
+  }
+}
+
 class ChangeflySplashScreen extends StatefulWidget {
   ChangeflySplashScreen({Key key}) : super(key: key);
 
@@ -15,9 +56,7 @@ class _ChangeflySplashScreenState extends State<ChangeflySplashScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'Changefly',
-            ),
+            ChangeflyName(),
           ],
         ),
       ),
